218902e403fe3f19d3aa9cad269acf04ff6ccd7d
André Fontes <andrefsfontes@gmail.com>
2023-09-11 23:18:07 -0300

    add css

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Andre Fontes Dev Pleno</title>
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+  <meta name="msapplication-TileColor" content="#2d89ef">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+
 </head>
 
 <body>
